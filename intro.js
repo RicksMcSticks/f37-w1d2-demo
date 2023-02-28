09c6e6725996e3587645459125348ee78c5d1aac
Cameron Ricks <cameron6x6@gmail.com>
2023-02-28 15:41:47 -0700

    added demo file

---
+++ intro.js
@@ -0,0 +1,34 @@
+
+
+//console.log("Hello, world")
+
+const firstName = "Cameron"
+
+//console.log(firstName)
+
+let age = 20
+
+const isCool = true 
+
+//console.log(firstName, age, isCool)
+
+//console.log("Hello, my name is " + firstName + " I am " + age + " years old.")
+
+
+
+let x = 4
+let y = 10
+let z = x + 8
+let sum = x + y + z
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+// console.log(sum)
+
+if (x === y){
+    console.log('X is and Y are the samw')
+} else {
+console.log('X and Y are different values')
+}
